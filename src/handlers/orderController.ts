e1d6fff3cb6f4dac067b11e8ff44e06e174f4f08
mohamedy72 <mohamedwebdev94@gmail.com>
2022-10-01 18:12:05 +0200

    refactor: No implicit any

--- src/handlers/orderController.ts
+++ src/handlers/orderController.ts
@@ -5,6 +5,7 @@ import {
   getSingleOrder,
   addPorductToOrder,
   addNewOrder,
+  Order,
 } from "../models/order";
 
 export const indexAllOrders = async (req: Request, res: Response) => {
@@ -15,7 +16,7 @@ export const indexAllOrders = async (req: Request, res: Response) => {
     });
   } catch (error) {
     res.status(400).json({
-      data: "Can't retrieve a list of orders",
+      data: `Can't retrieve a list of orders ${error}`,
     });
   }
 };
@@ -28,14 +29,14 @@ export const indexSingleOrder = async (req: Request, res: Response) => {
     });
   } catch (error) {
     res.status(400).json({
-      data: `Can't retrieve a list of orders, ${error}`,
+      data: `Can't retrieve an order with this id, ${error}`,
     });
   }
 };
 
 export const createNewOrder = async (req: Request, res: Response) => {
   try {
-    const newOrder = {
+    const newOrder: Order = {
       product_key: req.body.product_key,
       quantity: req.body.quantity,
       user_key: req.body.user_key,
@@ -64,7 +65,7 @@ export const insertPorductInOrder = async (req: Request, res: Response) => {
     });
   } catch (error) {
     res.status(400).json({
-      data: `Cannot retrieve the order with this id, ${error}`,
+      data: `Cannot retrieve add a product to this order, ${error}`,
     });
   }
 };
